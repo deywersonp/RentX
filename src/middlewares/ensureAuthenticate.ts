c980b873131af4dd159b4af7dd7b2c6da2318654
Deywerson Pereira <deywerson.pereira@gmail.com>
2021-08-16 19:13:01 -0300

    Created middleware for verify authentication / added error handling

---
+++ src/middlewares/ensureAuthenticate.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../modules/accounts/repositories/Implementations/UsersRepository";
+
+interface IPayLoad {
+  sub: string;
+}
+
+export async function ensureAuthenticate(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "f1e5ef4a94bec8b32d925341e7d3b932"
+    ) as IPayLoad;
+
+    const usersRepository = new UsersRepository();
+
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exists!", 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError("Invalid token", 401);
+  }
+}
